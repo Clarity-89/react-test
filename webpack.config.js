a2e9d972c65f18bb1ab4c35837e411510131a134
Clarity-89 <homes89@ukr.net>
2016-04-12 22:08:24 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+//const webpack = require('webpack');
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: './',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3333
+    },
+    
+    /* For production
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ],*/
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
